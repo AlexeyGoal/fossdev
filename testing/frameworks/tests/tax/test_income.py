986b201004ee42a82096d20492fc105fd4b6a5c5
alexei <sotnikovalexei844@gmail.com>
2026-03-05 15:06:43 +0800

    added test with unitest

--- testing/frameworks/tests/tax/test_income.py
+++ testing/frameworks/tests/tax/test_income.py
@@ -1,7 +1,7 @@
 from tax.income import calculate_tax
 
 def test_income_tax():
-    assert calculate_tax(100) ==15
+    assert calculate_tax(100) == 13
     print("Test tax passed")
 
 if __name__ == "__main__":
