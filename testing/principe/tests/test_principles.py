2e8f692609eec5b64a50808d49d43d8bdfeef42d
alexei <sotnikovalexei844@gmail.com>
2026-03-05 12:57:44 +0800

    added add_with_bug test

--- testing/principe/tests/test_principles.py
+++ testing/principe/tests/test_principles.py
@@ -1,11 +1,19 @@
-import sys
-sys.path.append("../src")
 
-from math_demo import add
+from math_demo import add, add_with_bug
+
 
 def test_addition():
-    assert 2+2 == 4
+    assert add(2,2) == 4
+    assert add(0,0) == 0
+    assert add(7,6) == 13
     print("Test ADDITION PASSED")
 
-if __name__ == "main":
+def test_addition_with_bug():
+    assert add_with_bug(2,2) == 4
+    assert add_with_bug(0,0) == 0
+    assert add_with_bug(7,6) == 13
+    print("Test BUGGED ADDITION PASSED")
+
+if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
