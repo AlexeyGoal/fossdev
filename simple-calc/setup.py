ce6215588929d1c250d70244f80405e8f5da97ef
alexei <sotnikovalexei844@gmail.com>
2026-03-31 19:52:22 +0800

    project

---
+++ simple-calc/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple-calc-kirill",
+    version="0.1.0",
+    author="Kirill",
+    description="Простой калькулятор с базовыми операциями",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/simple-calc",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "flake8>=3.9",
+            "black>=21.0",
+        ],
+    },
+)
